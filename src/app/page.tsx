4cbdfe5b31513a6072b6a83dfec95f33c35367be
Amzamul Haque <ahaquebd12@gmail.com>
2026-01-28 00:11:58 +0600

    core: update Home page -dark mode with design system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function HomePage() {
   return (
-    <section className="flex flex-col items-center justify-center h-full space-y-6">
-      <h1 className="text-2xl font-bold">Welcome to Ahmeet</h1>
-      <p className="text-base">
+    <section className="flex flex-col bg-[var-(--color-bg)] text-[var(--color-text)] items-center justify-center h-full space-y-6">
+      <h1 className="text-2xl font-bold text-[var(--color-text)]">
+        Welcome to Ahmeet
+      </h1>
+      <p className="text-[var(--color-muted)]">
         Enterprise-grade real-time video conferencing & collaboration.
       </p>
     </section>
