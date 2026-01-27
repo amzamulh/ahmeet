48a0dbb7d62240de794a814e3a20cc395e713314
Amzamul Haque <ahaquebd12@gmail.com>
2026-01-27 19:36:36 +0600

    components: add Footer.tsx with colro system

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="w-full p-4 text-center text-sm  border-t">
+        &copy; {new Date().getFullYear()} Ahmeet. All rights reserved.
+      </footer>
+    );
+  }
+  
