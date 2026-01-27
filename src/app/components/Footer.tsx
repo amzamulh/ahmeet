65b36e456ca6ed0a15a201bee638416b1ebc20e2
Amzamul Haque <ahaquebd12@gmail.com>
2026-01-28 00:17:48 +0600

    core: update Footer -dark mode with design system

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,7 @@
 export default function Footer() {
-    return (
-      <footer className="w-full p-4 text-center text-sm  border-t">
-        &copy; {new Date().getFullYear()} Ahmeet. All rights reserved.
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="w-full p-4 text-center text-[length:var(--text-sm)] text-[var(--color-muted)] border-t-2 border-[var(--color-border)] ">
+      &copy; {new Date().getFullYear()} Ahmeet. All rights reserved.
+    </footer>
+  );
+}
