4dfbb75411d543bde2f52172966427f1f7793900
Amzamul Haque <ahaquebd12@gmail.com>
2026-01-27 19:34:12 +0600

    components: add header.tsx with colro system

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Header() {
+  return (
+    <header className="w-full flex items-center justify-between p-4 shadow-sm">
+      <h1 className="text-xl font-bold ">
+        Ahmeet
+      </h1>
+    </header>
+  );
+}
