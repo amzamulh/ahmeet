92b1a6dbd06fa6f939b0c89f5ab0753fd6bcc1cb
Amzamul Haque <ahaquebd12@gmail.com>
2026-01-30 11:33:18 +0600

    core: update and move organisms foleder

---
+++ src/app/components/organisms/layouts/Header/Header.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Avatar } from "../../../atoms/Avatar";
+import { Button } from "../../../atoms/Button";
+import { Logo } from "../../../atoms/Logo";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-[var(--color-bg)] text-[var(--color-text)] flex items-center justify-between py-3 px-4 shadow-[var(--shadow-sm)] backdrop-blur-md border-b-1 border-[var(--color-border)]">
+      <div className="flex items-center h-full gap-2">
+        <Logo />
+      </div>
+      <nav className="flex items-center justify-end">
+        <div className="flex items-center gap-4">
+          <Button variant={"outline"}>Schedule</Button>
+          <Avatar name="Amzamul" />
+        </div>
+      </nav>
+    </header>
+  );
+}
