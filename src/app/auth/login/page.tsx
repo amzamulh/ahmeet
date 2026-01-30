214c12e51d004195e3188316bb73a46c22aefe5e
Amzamul Haque <ahaquebd12@gmail.com>
2026-01-31 00:22:57 +0600

    page: create login and register page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { AuthLayout } from "@/app/components/templates/Auth";
+const LoginPage = () => {
+  return (
+    <AuthLayout>
+      <h1 className="text-xl font-semibold text-center">Login form</h1>
+      {/* Form will be added later */}
+    </AuthLayout>
+  );
+};
+
+export default LoginPage;
