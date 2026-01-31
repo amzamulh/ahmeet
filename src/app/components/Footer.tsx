668ab114dbe29baf776efd66b4adfb9db1329332
Amzamul Haque <ahaquebd12@gmail.com>
2026-01-31 15:51:40 +0600

    delete: delete unnecessary files

--- src/app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full p-4 text-center text-[length:var(--text-sm)] text-[var(--color-muted)] border-t-2 border-[var(--color-border)] ">
-      &copy; {new Date().getFullYear()} Ahmeet. All rights reserved.
-    </footer>
-  );
-}
