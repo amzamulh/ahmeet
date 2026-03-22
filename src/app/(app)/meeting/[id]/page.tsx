4725da534081c60ddbd8f7c196d65fe7c3143f5a
Amzamul Haque <Md Amzamul Haque>
2026-03-22 10:08:22 +0600

    app: update meeting page

--- src/app/(app)/meeting/[id]/page.tsx
+++ src/app/(app)/meeting/[id]/page.tsx
@@ -1,22 +1,39 @@
+"use client";
+import { useDevicePermissions } from "@ah/hooks/devices/useDevicePermissions";
 import { ChatPanel } from "@ah/organisms/chat/ChatPanel";
 import { VideoGrid } from "@ah/organisms/confirence_room/VideoGrid";
 import { MediaControlBar } from "@ah/organisms/controls/MediaControlBar";
 import { Sidebar } from "@ah/organisms/layouts/Sidebar";
 import { MeetingLayout } from "@ah/templates/MeetingLayout";
+import { useEffect } from "react";
 
-interface MeetingPageProps {
-  params: { id: string };
-}
+const MeetingPage = () => {
+  const { requestPermissions, granted, micMissing, cameraMissing } =
+    useDevicePermissions();
+  if (granted) {
+    console.log("Permissions granted, proceeding to meeting...", granted);
+  }
 
-const MeetingPage = ({ params }: MeetingPageProps) => {
+  useEffect(() => {
+    const handleNewMeeting = async () => {
+      await requestPermissions();
+    };
+    handleNewMeeting();
+  }, []);
   return (
     <MeetingLayout
       sidebar={
-        <Sidebar isOpen>
+        <Sidebar isOpen={false}>
           <ChatPanel>chat panel</ChatPanel>
         </Sidebar>
       }
-      controls={<MediaControlBar />}
+      controls={
+        <MediaControlBar
+          isGranted={granted}
+          isMissingMic={micMissing}
+          isMissingCamera={cameraMissing}
+        />
+      }
     >
       <VideoGrid>Grid</VideoGrid>
     </MeetingLayout>
