924fdc677d262e19fa2366194db90cffd3500845
Amzamul Haque <Md Amzamul Haque>
2026-03-22 10:10:38 +0600

    app: update some templates

--- src/app/components/templates/MeetingLayout/MeetingLayout.tsx
+++ src/app/components/templates/MeetingLayout/MeetingLayout.tsx
@@ -1,10 +1,9 @@
 import { AppController } from "../../organisms/layouts/AppControler";
 import { Header } from "../../organisms/layouts/Header";
-import { Sidebar } from "../../organisms/layouts/Sidebar";
 
 export interface MeetingLayoutProps {
-  sidebar: React.ReactNode;
-  controls: React.ReactNode;
+  sidebar?: React.ReactNode;
+  controls?: React.ReactNode;
   children: React.ReactNode;
 }
 
@@ -14,10 +13,7 @@ export const MeetingLayout = ({
   children,
 }: MeetingLayoutProps) => {
   return (
-    <AppController
-      header={<Header />}
-      sidebar={<Sidebar isOpen>{sidebar}</Sidebar>}
-    >
+    <AppController header={<Header />} sidebar={sidebar}>
       <div className="flex h-full flex-col">
         <div className="flex-1 overflow-auto p-4">{children}</div>
         {controls}
